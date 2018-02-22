207df63a5e2edb2f88ddcec347a6bd1cebf81c2f
Boris Borisov <boris.r.borisov@gmail.com>
2018-02-22 10:54:35 +0200

    ignore bundle.js use stateless component instead of extending class Component

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-class App extends React.Component {
-    render() {
-        return <p>React simple starter</p>;
-    }
-}
+const App = () => {
+    return (
+        <p>React simple starter</p>
+    );
+};
 
 render(<App/>, document.getElementById('app'));
